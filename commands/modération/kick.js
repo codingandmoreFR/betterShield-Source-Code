478db498f430d4dcf39526bf09049f9ceb8539c4
Xecozz <94707887+Xecozz@users.noreply.github.com>
2022-05-30 19:34:17 +0200

    embed modify kick

--- commands/modération/kick.js
+++ commands/modération/kick.js
@@ -35,15 +35,17 @@ module.exports = {
         let embedNotKick = new MessageEmbed()
             .setColor('RANDOM')
             .setTitle("Expulsion")
-            .setDescription("Vous ne pouvez pas expulser ce membre !")
+            .setDescription("<:nope:973641602725736588> Vous ne pouvez pas expulser ce membre !<:nope:973641602725736588>")
+            .setFooter({ text: `Par ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({dynamic : true}) })
             .setTimestamp()
 
         if (!member.kickable) return await interaction.editReply({ embeds: [embedNotKick], ephemeral: true })
 
         let embedKick = new MessageEmbed()
             .setColor('RANDOM')
-            .setTitle(`Expulsion`)
-            .setDescription(`${interaction.user.tag} à expulsé ${member.user.tag}.\nRaison : ${raison}`)
+            .setTitle(`<a:above:973641634216546306> Expulsion Réussie `)
+            .setDescription(`${interaction.user.username} à expulsé ${member.user.username}.\nRaison : ${raison}`)
+            .setFooter({ text: `Par ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({dynamic : true}) })
             .setTimestamp()
         if(raison){
             await member.kick({reason: raison})
